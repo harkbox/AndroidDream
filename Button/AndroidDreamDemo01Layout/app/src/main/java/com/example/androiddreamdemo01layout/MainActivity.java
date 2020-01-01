6b3f9acdc133e07905783697772002ecbed431e9
harkbox <975364398.com>
2020-01-01 18:57:13 +0800

    简单圆形渐变按钮

---
+++ Button/AndroidDreamDemo01Layout/app/src/main/java/com/example/androiddreamdemo01layout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androiddreamdemo01layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//这是一个简单的布局界面，主要学习了控件阴影和圆角button
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 This is a android demo text.
 编译器是 android studio，如果大家也有许多很不错的学习demo，可以共享出来，一起学习。
 ## Button
-阴影效果博客：https://blog.csdn.net/qq_43702629/article/details/103793643
+layerListtext阴影效果博客：https://blog.csdn.net/qq_43702629/article/details/103793643
+AndroidDreamDemo01Layout:圆形button
+
